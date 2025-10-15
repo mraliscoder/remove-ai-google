4161da6d9f6862f8119fb0766984c28b94ae6728
Eduard Ilin <me@edwardcode.net>
2025-10-15 14:33:11 +1000

    Initial commit

---
+++ remove-ai-google.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name         Remove AI search from google
+// @namespace    http://tampermonkey.net/
+// @version      2025-10-15
+// @description  Remove AI search from Google
+// @author       edwardcode
+// @match        https://www.google.com/search*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=google.com
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+    const nodes = document.querySelectorAll('[role="listitem"]');
+    const aiModeNode = Array.from(nodes).find(node => {
+        return node.textContent.includes('AI') || node.textContent.includes('ИИ');
+    });
+
+    aiModeNode.remove();
+    });
+})();
